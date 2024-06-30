40f60fd2d10a349cad50caa24277f73ad57efd95
Shivcharan Tirkey <shivtirkey007@gmail.com>
2024-06-30 23:10:27 +0530

    1.2 Setting up

---
+++ src/main/java/com/shivtirkey/week1Introduction/introdutionToSpringBoot/IntrodutionToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.shivtirkey.week1Introduction.introdutionToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrodutionToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrodutionToSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shivtirkey/week1Introduction/introdutionToSpringBoot/IntrodutionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivtirkey.week1Introduction.introdutionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrodutionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
